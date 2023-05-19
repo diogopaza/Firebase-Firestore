24321e34e80047fd220df8ef1fb0d9ad6611954f
Diogo Paza <diogopazacvel@gmail.com>
2023-05-18 22:56:36 -0300

    crud firebase

---
+++ public/index.js
@@ -0,0 +1,49 @@
+document
+  .getElementById("postForm")
+  .addEventListener("submit", function (event) {
+    event.preventDefault();
+    const text = document.getElementById("postText").value;
+    const post = {
+      text: text,
+      user_id: "diogo",
+      likes: 0,
+      comments: [],
+    };
+    const postsCollection = firebase.firestore().collection("posts");
+    postsCollection.add(post).then(() => {
+      document.getElementById("postText").value = "";
+      loadPosts();
+    });
+  });
+
+function addPosts(post) {
+  const postTemplate = `
+    <li id="${post.id}">
+        ${post.data().text} ❤️ ${post.data().likes}
+    </li> `;
+  document.getElementById("posts").innerHTML += postTemplate;
+}
+
+function loadPosts() {
+  const postsCollection = firebase.firestore().collection("posts");
+  document.getElementById("posts").innerHTML = "Carregando ...";
+  postsCollection.get().then((snap) => {
+    document.getElementById("posts").innerHTML = "";
+    snap.forEach((doc) => {
+      console.log(doc.id);
+      addPosts(doc);
+    });
+  });
+}
+
+function deletePost(postId) {
+  const post = firebase.firestore().collection("posts").doc(postId);
+  post
+    .delete()
+    .then(() => {
+      loadPosts();
+    })
+    .catch((error) => {
+      console.log("Erro ao excluir post");
+    });
+}
